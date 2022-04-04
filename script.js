c466d9ec8961c898b13db4ca15719710bf4b771f
ingridlo <ingridloargote@gmail.com>
2022-04-04 16:06:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const totalCuenta= document.getElementById("totalC");
 const propina= document.getElementById("prop");
 const totalPagar= document.getElementById("pagar");
 const boton = document.getElementById("calcular");
+const form = document.getElementById("form")
 let contador =0
 
-total1.addEventListener("click",()=>{
-    total1.value= "";
-    porcentaje.value="";
-})
 
-
-boton.addEventListener("click", ()=>{
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()    
     totalCuenta.innerHTML=("$ " + total1.value)
     propina.innerHTML= ("$ " + porcentajeCalculador (total1.value,porcentaje.value)[0])
     totalPagar.innerHTML= ("$ " + porcentajeCalculador (total1.value,porcentaje.value)[1])
+    borrar()   
 })
 
+
+function borrar() {
+    form.reset()
+}
+
+total1.addEventListener("click",()=>{    
+    totalCuenta.innerHTML='';
+    propina.innerHTML='';
+    totalPagar.innerHTML='';
+})
 
 function porcentajeCalculador(num1,num2){    
     let porcentaje = (num1*num2)/100        
